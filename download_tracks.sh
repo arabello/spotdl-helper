383339c59b1c63d4b5deda81a5913b353db11f05
Matteo Pellegrino <matteo.pellegrino@buildo.io>
2024-07-12 21:21:39 +0200

    feat: add support for yt-music cookie file

--- download_tracks.sh
+++ download_tracks.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <config_file>"
-    exit 1
+if [ $# -lt 1 ] || [ $# -gt 2 ]; then
+  echo "Usage: $0 <config_file> [cookie_file]"
+  exit 1
 fi
 
 CONFIG_FILE=$1
+COOKIE_FILE=$2
 BASE_NAME=$(basename "$CONFIG_FILE" .yml)
 ROOT_DIR="$BASE_NAME"
 
@@ -26,12 +27,17 @@ process_playlists() {
       # It's a string, handle playlist based on the existence of the sync file
       pushd "$folder" > /dev/null
       sync_file="$key.sync.spotdl"
+      spotdl_args=(--threads 8 --print-errors --format mp3)
+      if [ -n "$COOKIE_FILE" ]; then
+        echo "Using cookie file"
+        spotdl_args+=(--cookie-file "$COOKIE_FILE")
+      fi
       if [ ! -f "$sync_file" ]; then
         echo "Init "$folder""
-        spotdl sync "$value" --save-file "$sync_file" --threads 8 --print-errors
+        spotdl sync "$value" "${spotdl_args[@]}" --save-file "$sync_file"
       else
         echo "Sync "$folder""
-        spotdl sync "$sync_file" --threads 8 --print-errors
+        spotdl sync "$sync_file" "${spotdl_args[@]}" 
       fi
       popd > /dev/null
     fi
@@ -40,18 +46,18 @@ process_playlists() {
 
 # Check if yq is installed
 if command -v yq &> /dev/null; then
-    if [ -f "$CONFIG_FILE" ]; then
-        # Read the top-level keys and start recursion
-        root_dir="$ROOT_DIR"
-        yaml_content=$(yq eval "." "$CONFIG_FILE")
+  if [ -f "$CONFIG_FILE" ]; then
+    # Read the top-level keys and start recursion
+    root_dir="$ROOT_DIR"
+    yaml_content=$(yq eval "." "$CONFIG_FILE")
 
-        mkdir -p "$root_dir"
-        process_playlists "$root_dir" "$yaml_content"
-    else
-        echo "Configuration file not found: $CONFIG_FILE"
-        exit 1
-    fi
-else
-    echo "Please install yq to parse the configuration file."
+    mkdir -p "$root_dir"
+    process_playlists "$root_dir" "$yaml_content"
+  else
+    echo "Configuration file not found: $CONFIG_FILE"
     exit 1
+  fi
+else
+  echo "Please install yq to parse the configuration file."
+  exit 1
 fi
