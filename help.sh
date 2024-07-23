0d1e2db119e47d440e868a4d0b5dfb09eb4c776b
Matteo Pellegrino <matteo.pellegrino@buildo.io>
2024-07-23 12:47:58 +0200

    yo

--- help.sh
+++ help.sh
@@ -1,8 +1,10 @@
+echo "AUTH_TOKEN=\$(./get_spotify_token.sh \$SP_CLIENT_ID \$SP_CLIENT_SECRET)"
+
 echo "spotdl sync <playlist-url> --save-file <playlist-name>.sync.spotdl \\
---threads 16 \\
+--threads 8 \\
 --cookie-file <cookie-file> \\
 --use-cache-file \\
---auth-token \$(./get_spotify_token.sh <client_id> <client_secret>)"
+--auth-token \$AUTH_TOKEN"
 
 echo 
 echo "for syncing remove <playlist-url> and --save-file"
